40effb548fc2b6518d2fa45593e1d16a8a4259e6
shrikantchand <92122205+shrikantchand@users.noreply.github.com>
2025-07-21 18:10:57 -0400

    Update recon_sst.py

--- recon_sst.py
+++ recon_sst.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 def recon_sst(tfrsq, tfrsqtic, Hz, c, Band, coeff):
+# Reconstructs asignal using its synchrosqueezed transform's curves.
+# INPUT
+#    tfrsq     :  Synchrosqueexed TFR.
+#    tfrsqtic  :  Frequency axis.
+#    Hz        :  Sampling rate (in Hz).
+#    c         :  Extracted curve.
+#    Band      :  Band width for curve.
+#    coeff     :  Frequency resolution coefficient.
+
+# OUTPUT       :  Reconstructed signal
+# Adapted from the MATLAB implementation by Hau-Tieng Wu into Python by Shrikant Chand (shrikant.chand@duke.edu) and Po-Ying Chen in 04.2024.
     alpha = tfrsqtic[1] - tfrsqtic[0]
     RR = round(Band / (Hz * alpha))
     recon = []
